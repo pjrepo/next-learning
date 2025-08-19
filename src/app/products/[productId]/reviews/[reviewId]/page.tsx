e84ca8180a688f68f1c3bbfff1dbadf09fc64a6a
pjrepo <pavanjegurupati75@gmail.com>
2025-08-19 16:45:44 +0530

    "not found"

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,9 +1,15 @@
+import { notFound } from "next/navigation";
+
 const ProductReview = async ({
   params,
 }: {
   params: Promise<{ productId: string; reviewId: string }>;
 }) => {
   const { productId, reviewId } = await params;
+
+  if (parseInt(reviewId) > 1000) {
+    notFound();
+  }
 
   return (
     <h1>
